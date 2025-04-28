5e7a57fd39a940c2c75cc307f3b93200f6ce2fb6
Stijn De Pauw <stijn-depauw@hotmail.be>
2025-04-28 19:23:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# STORM: Single Table ORM for DynamoDB
+
+TL;DR: A Django-like interface for interacting with DynamoDB objects.
---
+++ base/connection_test_utils.py
@@ -0,0 +1,72 @@
+import os
+import boto3
+import botocore
+import pytest
+
+from base.connection import table
+
+TABLE_NAME = "unit-test-table"
+# Must reflect actual table definition
+TABLE_DEFINITION = {
+    "TableName": TABLE_NAME,
+    "KeySchema": [
+        {"AttributeName": "PK", "KeyType": "HASH"},
+        {"AttributeName": "SK", "KeyType": "RANGE"},
+    ],
+    "AttributeDefinitions": [
+        {"AttributeName": "PK", "AttributeType": "S"},
+        {"AttributeName": "SK", "AttributeType": "S"},
+        {"AttributeName": "GSI1PK", "AttributeType": "S"},
+    ],
+    "GlobalSecondaryIndexes": [
+        {
+            "IndexName": "GSI-1",
+            "KeySchema": [
+                {"AttributeName": "GSI1PK", "KeyType": "HASH"},
+                {"AttributeName": "SK", "KeyType": "RANGE"},
+            ],
+            "Projection": {"ProjectionType": "ALL"},
+            "ProvisionedThroughput": {
+                "ReadCapacityUnits": 1,
+                "WriteCapacityUnits": 1,
+            },
+        }
+    ],
+    "ProvisionedThroughput": {"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
+}
+
+
+@pytest.fixture()
+def local_client():
+    print("Setting up local DynamoDB client")
+    host = os.getenv("DYNAMODB_HOST", "127.0.0.1")
+    port = os.getenv("DYNAMODB_PORT", "8000")
+    endpoint_url = f"http://{host}:{port}"
+
+    # The key id and access key are fake, but they are required to
+    # connect to the local DynamoDB instance.
+    client = boto3.client(
+        "dynamodb",
+        region_name="us-east-1",
+        aws_access_key_id="Fake",
+        aws_secret_access_key="Fake",
+        endpoint_url=endpoint_url,
+    )
+
+    try:
+        client.create_table(
+            **TABLE_DEFINITION,
+        )
+        waiter = client.get_waiter("table_exists")
+        waiter.wait(TableName=TABLE_NAME)
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "ResourceInUseException":
+            print("Table already exists")
+        else:
+            raise
+
+    # Set the connection to the test client
+    with table.table_context(table_name=TABLE_NAME, client=client):
+        yield client
+    # Clean up any resources
+    client.delete_table(TableName=TABLE_NAME)
