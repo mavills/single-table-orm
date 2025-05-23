508dcdb87ec54bd5eb81ff3255c3b23503cb7f3e
Stijn De Pauw <stijn-depauw@hotmail.be>
2025-05-23 16:24:01 +0200

    Make the table part of the design

--- tests/utils.py
+++ tests/utils.py
@@ -6,39 +6,11 @@ from moto import mock_aws
 
 from single_table_orm import connection
 from single_table_orm.connection import table  # Updated import
+from single_table_orm.table_definition import get_starndard_definition
 
 TABLE_NAME = "unit-test-table"
 # Must reflect actual table definition
-TABLE_DEFINITION = {
-    "TableName": TABLE_NAME,
-    "KeySchema": [
-        {"AttributeName": "PK", "KeyType": "HASH"},
-        {"AttributeName": "SK", "KeyType": "RANGE"},
-    ],
-    "AttributeDefinitions": [
-        {"AttributeName": "PK", "AttributeType": "S"},
-        {"AttributeName": "SK", "AttributeType": "S"},
-        {"AttributeName": "GSI1PK", "AttributeType": "S"},
-    ],
-    "GlobalSecondaryIndexes": [
-        {
-            # Corrected IndexName to match query logic
-            "IndexName": "GSI-1",
-            "KeySchema": [
-                {"AttributeName": "GSI1PK", "KeyType": "HASH"},
-                # Corrected GSI KeySchema: SK should be part of GSI1
-                # No, SK can be RANGE key for GSI. Let's assume it's correct.
-                {"AttributeName": "SK", "KeyType": "RANGE"},
-            ],
-            "Projection": {"ProjectionType": "ALL"},
-            "ProvisionedThroughput": {
-                "ReadCapacityUnits": 1,
-                "WriteCapacityUnits": 1,
-            },
-        }
-    ],
-    "ProvisionedThroughput": {"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
-}
+TABLE_DEFINITION = get_starndard_definition(TABLE_NAME)
 
 
 @pytest.fixture(scope="function")
